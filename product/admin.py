050f7bd9cf9fb19307d7e2e9ac8720aecf427083
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2021-01-08 16:24:41 +0200

    first commit

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
-from .models import Category, Product, Images, Comment
-from mptt.admin import DraggableMPTTAdmin
 import admin_thumbnails
-class CategoryAdmin(admin.ModelAdmin):
-	list_display = ['title', 'parent', 'status']
-	list_filter = ['status']
+from django.contrib import admin
+
+# Register your models here.
+from mptt.admin import DraggableMPTTAdmin
+
+from product import models
+from product.models import Category, Product, Images, Comment, Color, Size, Variants
+
 
 class CategoryAdmin2(DraggableMPTTAdmin):
     mptt_indent_field = "title"
@@ -12,7 +14,7 @@ class CategoryAdmin2(DraggableMPTTAdmin):
                     'related_products_count', 'related_products_cumulative_count')
     list_display_links = ('indented_title',)
     prepopulated_fields = {'slug': ('title',)}
-    
+    #inlines = [CategoryLangInline]
     def get_queryset(self, request):
         qs = super().get_queryset(request)
 
@@ -40,27 +42,51 @@ class CategoryAdmin2(DraggableMPTTAdmin):
         return instance.products_cumulative_count
     related_products_cumulative_count.short_description = 'Related products (in tree)'
 
+@admin_thumbnails.thumbnail('image')
 class ProductImageInline(admin.TabularInline):
     model = Images
-    extra = 5
+    readonly_fields = ('id',)
+    extra = 1
 
-class ProductAdmin(admin.ModelAdmin):
-	list_display = ['title', 'category', 'status','image_tag']
-	list_filter = ['category']
-	readonly_fields = ('image_tag',)
-	inlines = [ProductImageInline]
+class ProductVariantsInline(admin.TabularInline):
+    model = Variants
+    readonly_fields = ('image_tag',)
+    extra = 1
+    show_change_link = True
+
 
 @admin_thumbnails.thumbnail('image')
 class ImagesAdmin(admin.ModelAdmin):
     list_display = ['image','title','image_thumbnail']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','category', 'status','image_tag']
+    list_filter = ['category']
+    readonly_fields = ('image_tag',)
+    inlines = [ProductImageInline,ProductVariantsInline]
+    prepopulated_fields = {'slug': ('title',)}
+
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['subject','comment', 'status','create_at']
     list_filter = ['status']
     readonly_fields = ('subject','comment','ip','user','product','rate','id')
 
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ['name','code','color_tag']
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name','code']
+
+
+class VariantsAdmin(admin.ModelAdmin):
+    list_display = ['title','product','color','size','price','quantity','image_tag']
+
+
 admin.site.register(Category,CategoryAdmin2)
-admin.site.register(Product, ProductAdmin)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Comment,CommentAdmin)
 admin.site.register(Images,ImagesAdmin)
-# Register your models here.
+admin.site.register(Color,ColorAdmin)
+admin.site.register(Size,SizeAdmin)
+admin.site.register(Variants,VariantsAdmin)
